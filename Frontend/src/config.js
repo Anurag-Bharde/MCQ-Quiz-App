5f6ff81c750a7305fe25dc72e20587c6f9bd40a4
Anurag-Bharde <anwolfragspitter99@gmail.com>
2024-10-14 01:07:01 +0530

    frontend changess

--- Frontend/src/config.js
+++ Frontend/src/config.js
@@ -1,2 +1,2 @@
-export const Back_User = "https://mcq-quiz-app-backend.vercel.app/User/";
-export const Back_Quiz = "https://mcq-quiz-app-backend.vercel.app/Quiz/";
+export const Back_User =  process.env.REACT_APP_BACK_USER || "https://mcq-quiz-app-backend.vercel.app/User/";
+export const Back_Quiz =  process.env.REACT_APP_BACK_QUIZ || "https://mcq-quiz-app-backend.vercel.app/Quiz/";
