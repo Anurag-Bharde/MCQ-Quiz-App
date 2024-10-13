af4f19c9d35d25f1f7d4612ce197e7d6a5c84bf0
Anurag-Bharde <anwolfragspitter99@gmail.com>
2024-10-14 00:48:27 +0530

    changes

--- backend/index.js
+++ backend/index.js
@@ -5,7 +5,6 @@ const bodyparser=require('body-parser');
 const UserRouter=require("./Routes/UserInf")
 const QuizRoute=require("./Routes/Quiz")
 const cors=require("cors");
-const PORT= process.env.PORT || 3000
 
 app.use(cors());
 app.use(express.json());
@@ -15,7 +14,4 @@ app.use(bodyparser.json());
 app.use("/User",UserRouter);
 app.use("/Quiz",QuizRoute);
 
-
-app.listen(PORT, () => {
-    console.log("Server running on port "+ PORT    );
-  });
+module.exports = app
